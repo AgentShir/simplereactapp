2704487c5798cfde33db65b4f6ae1405746f5b27
Shireen Garcia <garciasal@gmail.com>
2017-08-27 23:13:46 -0700

    Everything displays correctly, FINISHED

--- src/data/EarthquakeList.js
+++ src/data/EarthquakeList.js
@@ -2,39 +2,37 @@ import React, { Component } from 'react';
 import earthquakes from '../data/earthquakes.js';
 import moment from 'moment';
 
-let info = earthquakes.features.map(earthquakes => {
-  return (
-        <div className="quake-list">
-          <div className="row">
-          {earthquakes.map(earthquakes => (
-            <div className="col-sm-6" key={earthquakes.place}>
-              <div className="card" >
-                <div className="card-block">
-                  <h4 className="card-title">{earthquakes.title}</h4>
-                  <h6 className="card-subtitle mb-2 text-muted">Magnitude: {earthquakes.properties.mag}</h6>
-                  <h6 className="card-subtitle mb-2 text-muted">Time: {moment(earthquakes.time).format('llll')}</h6>
-                  <p className="card-text">Coordinates: {earthquakes.geometry.coordinates}</p>
-
-                  <a href={earthquakes.properties.url} className="card-link">USGS Event Link</a>
-                </div>
-              </div>
-            </div>
-          )
-        )
-      }
-      </div>
-    </div>
-  )
-    });
-
 class EarthquakeList extends Component{
-  render(){
+	render(){
+	let info = earthquakes.features.map((earthquakes => {
+	  return (
+	        <div className="quake-list">
+	          <div className="row">
+	            <div className="col-sm-6" key={earthquakes.place}>
+	              <div className="card" >
+	                <div className="card-block">
+	                  <h4 className="card-title">{earthquakes.title}</h4>
+	                  <h6 className="card-subtitle mb-2 text-muted">Magnitude: {earthquakes.properties.mag}</h6>
+	                  <h6 className="card-subtitle mb-2 text-muted">Time: {moment(earthquakes.time).format('llll')}</h6>
+	                  <p className="card-text">Coordinates: {earthquakes.geometry.coordinates}</p>
+
+	                  <a href={earthquakes.properties.url} className="card-link">USGS Event Link</a>
+	                </div>
+	              </div>
+	            </div>
+	      		}
+	      	</div>
+	    	</div>
+	  		)
+	  })
+	)
     return(
       <section>
         {info}
       </section>
     )
-  }
+
+	}
 }
 
 export default EarthquakeList;
